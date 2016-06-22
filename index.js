7822f33b7d4353bb4f51e780d4c85e6507c22cf1
Jason Tang <jasont@emetric.net>
2016-06-22 15:57:20 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+module.exports = dui;
+
+function dui(options, callback) {
+  //options.apos: the apos object, a singleton which provides core methods for content management
+  //options.app: the Express app object.  You can add routes via app.get, app.post, etc. at any time in your module's constructor
+  //options.pages: the pages object, a singleton which provides methods for dealing with the page tree
+  //options.schemas: the schemas object, a singleton which provides methods for dealing with schemas
+  //options.mailer: a nodemailer transport object, ready to send email as needed
+  //options.site: an object containing title, shortName and hostName properties, as configured in app.js.
+ 
+  return new DragonUI(options, callback);
+}
+
+function DragonUI(options, callback) {
+  var self = this;
+  // Add a bunch of methods to self here, then...
+  self._apos = options.apos;
+  self._app = options.app;
+  self._pages = options.pages;
+  self._schemas = options.schemas;
+  self._options = options;
+    // Mix in the ability to serve assets and templates
+  self._apos.mixinModuleAssets(self, 'dragon-ui', __dirname, options);
+  
+  self.pushAsset('stylesheet', 'dragon', { when: 'always' });
+
+  var pages = {
+    'home': 'home',
+    'default': 'default',
+    'blog': 'blog'
+  };
+
+  self.loader = function(req, callback) {
+
+    if (pages[req.page.type]) {
+      //support the default demo page types
+      //default, home, blog
+      req.template = self.renderer(pages[req.page.type], {});
+    }
+    console.log("page:" + JSON.stringify(req.page));
+
+    return callback(null);
+  };
+
+  // Invoke the callback. This must happen on next tick or later!
+  if (callback) {
+    return process.nextTick(function() {
+      return callback(null);
+    });
+  }
+}
+
+// Export the constructor so others can subclass
+dui.DragonUI = DragonUI;
